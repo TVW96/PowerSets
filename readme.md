e7de8d8682ff7a9552b12ec781831dd5d64aa6a7
Troy Washington <troyvw96@gmail.com>
2024-10-15 14:26:33 -0700

    update readme.md file

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Generate Power Sets
+
+### Given `User Input` the application will create a set, generate a power set, and display it's results.
+
+#### Set = *S*
+#### PowerSer = *P(S)*
+
+### Example:
+
+if input *S* = {a, b, c},
+
+then output *P(S)* = {[], [a], [b], [a, b], [c], [a, c], [b, c], [a, b, c]}
+
+
+## Instructions:
+To run the project press the green error at the top of you IDE. 
+The first input ask's for set length `Number of set elements: `, this is unput is followed by input request for according element index `Set element 1: a`, `Set element 2: b`, `Set element 3: c`. After inputting the final set the generated Power Set result will display: 
+```
+Power Set: {[], [a], [b], [a, b], [c], [a, c], [b, c], [a, b, c]}
+```
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
+
+    public static <T> List<List<T>> generatePowerSet(List<T> list) {
+        List<List<T>> powerSet = new ArrayList<>();
+
+        int setSize = list.size();
+        int powerSetSize = (int) Math.pow(2, setSize);
+
+        for (int i = 0; i < powerSetSize; i++) {
+            List<T> subset = new ArrayList<>();
+            for (int j = 0; j < setSize; j++) {
+                if ((i & (1 << j)) != 0) {
+                    subset.add(list.get(j));
+                }
+            }
+            powerSet.add(subset);
+        }
+        return powerSet;
+    }
+
+    public static void printPowerSet(List<List<String>> powerSet) {
+        System.out.print("Power Set: {");
+        for (int i = 0; i < powerSet.size(); i++) {
+            List<String> subset = powerSet.get(i);
+            System.out.print(subset);
+            if (i < powerSet.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number of set elements: ");
+
+        int setSize = scanner.nextInt();
+        List<String> inputSet = new ArrayList<>();
+
+        for (int i = 0; i < setSize; i++) {
+            System.out.println("Set element " + (i + 1) + ":");
+            inputSet.add(scanner.next());
+        }
+        List<List<String>> powerSet = generatePowerSet(inputSet);
+        printPowerSet(powerSet);
+
+        scanner.close();
+    }
 }
